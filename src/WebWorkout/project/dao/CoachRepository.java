33d497e315c481f36d63cfc7ad85b84bb7a02b36
HPspect <yasenusa3@gmail.com>
2022-04-01 12:59:16 +0300

    Third Version Project

--- src/WebWorkout/project/dao/CoachRepository.java
+++ src/WebWorkout/project/dao/CoachRepository.java
@@ -2,13 +2,12 @@ package WebWorkout.project.dao;
 
 import WebWorkout.project.exception.NoneexistingEntityException;
 import WebWorkout.project.model.Coach;
-import WebWorkout.project.model.Workout;
 
 public interface CoachRepository extends Repository<Long, Coach> {
 
     // create coach
     Coach create(Coach entity);
-    void update(Coach entity) throws NoneexistingEntityException;
+    Coach update(Coach entity) throws NoneexistingEntityException;
     Coach deleteById(Coach id) throws NoneexistingEntityException;
     long count();
 }
